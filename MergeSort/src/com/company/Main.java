dec8485cea63d01503e29350a733dd69f8e8e050
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-12 21:08:54 -0600

    Updated

--- MergeSort/src/com/company/Main.java
+++ MergeSort/src/com/company/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        //int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        int[] intArray = {1,3,5,2,4,6};
 
         mergeSort(intArray,0,intArray.length);
 
         for(int i = 0;i<intArray.length;i++)
         {
-            System.out.println(intArray[i]);
+            System.out.print(intArray[i] + " ");
         }
     }
 
@@ -40,12 +41,17 @@ public class Main {
       int j = mid;
       int tempIndex = 0;
 
-
+        int counter = 0;
       int[] temp = new int[end-start];
       while(i<mid && j<end)
       {
+          if(input[j]<input[i])
+          {
+              counter++;
+          }
           temp[tempIndex++] = input[i]<=input[j]?input[i++]:input[j++];
       }
+      System.out.println("Counter val is :" + counter);
 
       //To handle any elements that are left in the left array, we need to copy that element to the right side
       //If any elements are left on the right hand side, they need not be handled as they will be overwritten with themselves if they are handled
@@ -55,18 +61,6 @@ public class Main {
       //The temp array should be copied to input array
       System.arraycopy(temp,0,input,start,tempIndex);
       //Here, the source array is temp, should be from 0, to input array, starting from start, the length to copy is tempIndex
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }

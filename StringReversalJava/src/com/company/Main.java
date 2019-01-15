408e59d2828110dfab889718ead515492fd8377c
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-14 23:39:53 -0600

    Updated the String Words Reverse Program

--- StringReversalJava/src/com/company/Main.java
+++ StringReversalJava/src/com/company/Main.java
@@ -26,4 +26,14 @@ public class Main {
 
         return s2;
     }
+
+
+
+    /*public String reverseWords(String s) {
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++) str[i] = new StringBuilder(str[i]).reverse().toString();
+        StringBuilder result = new StringBuilder();
+        for (String st : str) result.append(st + " ");
+        return result.toString().trim();
+    }*/
 }

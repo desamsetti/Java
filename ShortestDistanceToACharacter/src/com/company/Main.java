fe4e0c371d2a4872ec864996b1c65428bc2c5503
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-15 22:47:52 -0600

    ShortestDistanceToACharacter

--- ShortestDistanceToACharacter/src/com/company/Main.java
+++ ShortestDistanceToACharacter/src/com/company/Main.java
@@ -15,20 +15,25 @@ public class Main {
 
     public static int[] shortestToChar(String S, char C) {
         int n = S.length();
+        System.out.println("The size of the String is "+n);
         int[] res = new int[n];
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
         {
             res[i] = S.charAt(i) == C ? 0 : n;
         }
 
-        for (int i = 1; i < n; ++i)
+        for (int i = 1; i < n; i++)
         {
             res[i] = Math.min(res[i], res[i - 1] + 1);
         }
+
         for (int i = n - 2; i >= 0; --i)
         {
             res[i] = Math.min(res[i], res[i + 1] + 1);
         }
+
+
+
         return res;
     }
 

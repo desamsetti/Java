abef02b16b817cf205f0053ac3ab19cfec26b736
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-15 23:30:01 -0600

    Updated SingleDigitNumberDivisonCounting

--- SingleDigitNumberDivisionCounting/src/com/company/Main.java
+++ SingleDigitNumberDivisionCounting/src/com/company/Main.java
@@ -2,13 +2,28 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+* 1.  39 = 3*9 = 27
+2.  27 = 2*7 = 14
+3.  14 = 1*4 = 4
 
+1.  45 = 4*5 = 20
+2.  20 = 2*0 = 0
+
+1.  49 = 4*9 = 36
+2.  36 = 3*6 = 18
+3.  18 = 1*8 = 8
+
+1.  729 = 7*2*9 = 126
+2.  126 = 1*2*6 = 12
+3.  12  = 1*2 = 2
+
+* */
 public class Main {
     public static int counter = 0;
     public static void main(String[] args) {
 	// write your code here
         System.out.print(getNumber(999));
-
     }
 
 
@@ -33,11 +48,10 @@ public class Main {
         {
             getNumber(multiplyVal);
         }
-
-        if(String.valueOf(multiplyVal).length()==1)
+        /*if(String.valueOf(multiplyVal).length()==1)
         {
             return counter;
-        }
+        }*/
         return counter;
     }
 }

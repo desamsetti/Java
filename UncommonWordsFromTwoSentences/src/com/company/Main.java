40f485152f4062e5010e79133819b6da0d76c317
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-25 22:43:21 -0600

    Added UncommonWordsFromTwoSentences

---
+++ UncommonWordsFromTwoSentences/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+    public static String[] uncommonFromSentences(String A, String B) {
+        String[] listA = A.split("");
+        String[] listB = B.split("");
+
+        for(int i=0;i<listA.length;i++)
+        {
+            for(int j=0;j<listB.length;j++)
+            {
+            }
+        }
+    }
+}

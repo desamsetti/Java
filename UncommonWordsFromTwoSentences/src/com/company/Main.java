b98b36b22d20bbb39db8e77bda2978296b26ae4d
Harshith Desamsetti <dharshith1996@gmail.com>
2019-01-25 23:03:34 -0600

    Updated UncommonWordsFromTwoSentences

--- UncommonWordsFromTwoSentences/src/com/company/Main.java
+++ UncommonWordsFromTwoSentences/src/com/company/Main.java
@@ -1,20 +1,47 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String[] a = uncommonFromSentences("hello how are you?","hello how are you? Sir");
+        for(String s : a)
+        {
+            System.out.print(s +" ");
+        }
 
     }
     public static String[] uncommonFromSentences(String A, String B) {
-        String[] listA = A.split("");
-        String[] listB = B.split("");
+        HashSet<String> m1 = new HashSet<String>();
+        HashSet<String> m2 = new HashSet<String>();
 
-        for(int i=0;i<listA.length;i++)
+        for(String s:A.split(""))
         {
-            for(int j=0;j<listB.length;j++)
-            {
-            }
+            check(s,m1,m2);
         }
+        for(String s:B.split(""))
+        {
+            check(s,m1,m2);
+        }
+        return m1.toArray(new String[m1.size()]);
+
+
+    }
+
+
+
+    public static void check(String s, Set<String> m1, Set<String> m2)
+    {
+        if(m1.contains(s) && !m2.contains(s)){
+            m2.add(s);
+            m1.remove(s);
+        }
+        else if( !m2.contains(s) && !m1.contains(s)) m1.add(s);
+
     }
 }

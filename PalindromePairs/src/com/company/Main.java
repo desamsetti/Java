98afd01cce8154372c6103d6d30cc425c01a310d
Harshith Desamsetti <dharshith1996@gmail.com>
2019-02-02 11:36:43 -0600

    Palindrome Pairs Program using HashMap

--- PalindromePairs/src/com/company/Main.java
+++ PalindromePairs/src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -13,9 +15,10 @@ public class Main {
 
     }
 
-    public static List<List<Integer>> palindromePairs(String[] words) {
+    /*public static List<List<Integer>> palindromePairs(String[] words) {
         String word = "";
         List<List<Integer>> l1 = new ArrayList<List<Integer>>();
+        if (words == null || words.length < 2) return l1;
         for(int i=0;i<words.length-1;i++)
         {
             for(int j=i+1;j<words.length;j++)
@@ -37,10 +40,71 @@ public class Main {
             }
         }
         return l1;
+    }*/
+
+    public static List<List<Integer>> palindromePairs(String[] words) {
+        String word = "";
+        List<List<Integer>> l1 = new ArrayList<List<Integer>>();
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        if (words == null || words.length < 2) return l1;
+        for(int i=0;i<words.length;i++)
+        {
+            map.put(words[i],i);
+        }
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=0;j<words[i].length();j++)
+            {
+                String s1 = words[i].substring(0,j);
+                String s2 = words[i].substring(j);
+                if(checkPalindrome(s1))
+                {
+                    String str2rvs = new StringBuilder(s2).reverse().toString();
+                    if(map.containsKey(str2rvs) && map.get(str2rvs)!=i)
+                    {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(map.get(str2rvs));
+                        list.add(i);
+                        l1.add(list);
+                    }
+                }
+                if(checkPalindrome(s2))
+                {
+                    String str1rvs = new StringBuilder(s1).reverse().toString();
+                    if(map.containsKey(str1rvs) && map.get(str1rvs)!=i)
+                    {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(map.get(str1rvs));
+                        list.add(i);
+                        l1.add(list);
+                    }
+                }
+            }
+        }
+        return l1;
     }
 
 
-    public static boolean checkPalindrome(String sentence1,String sentence2)
+    public static boolean checkPalindrome(String sentence)
+    {
+        String checkSentence = "";
+        for(int i=sentence.length()-1;i>=0;i--)
+        {
+            checkSentence += sentence.charAt(i);
+        }
+        if(checkSentence.equalsIgnoreCase(sentence))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+    /*public static boolean checkPalindrome(String sentence1,String sentence2)
     {
         String sentence = sentence1+sentence2;
         String checkSentence = "";
@@ -56,5 +120,5 @@ public class Main {
         {
             return false;
         }
-    }
+    }*/
 }

04902cdfb6994340a8eb6ee6ec0e11c5c3d5cbee
Harshith Desamsetti <dharshith1996@gmail.com>
2019-04-17 16:44:01 -0500

    Updated Permutations Java Program

--- SquaresOfASortedArray/src/com/company/Main.java
+++ SquaresOfASortedArray/src/com/company/Main.java
@@ -15,11 +15,25 @@ public class Main {
             System.out.println(i);
         }
     }
-    public static int[] sortedSquares(int[] A) {
+    /*public static int[] sortedSquares(int[] A) {
         for(int i=0;i<A.length;i++){
             A[i] = A[i]*A[i];
         }
         Arrays.sort(A);
         return A;
+    }*/
+    public static int[] sortedSquares(int[] A) {
+        int result[] = new int[A.length];
+        int i = 0;
+        int j = A.length - 1;
+        int index = A.length - 1;
+        while(i <= j) {
+            if(Math.abs(A[i]) > Math.abs(A[j]))
+                result[index--] = A[i] * A[i++];
+            else
+                result[index--] = A[j] * A[j--];
+        }
+
+        return result;
     }
 }

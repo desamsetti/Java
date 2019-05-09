b57d9e3d2a6d0e748574d345e91fe36c301d2cd6
Harshith Desamsetti <dharshith1996@gmail.com>
2019-05-09 14:54:15 -0500

    Updated

---
+++ Udemy/Classes/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car porsche = new Car();
+        porsche.setModel("911");
+        System.out.println("Model is :" +porsche.getModel());
+    }
+}

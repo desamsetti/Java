54eff9afca8f86190cead93e5061d0fda62c1ba9
Harshith Desamsetti <dharshith1996@gmail.com>
2019-05-27 23:05:19 -0500

    Added CoinChangeDynamicProgrammingBottomUp Java Program

---
+++ CoinChangeDynamicProgramming/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int amount = 11;
+        int[] coins = {1,2,5};
+        int output = minimumCoinsRequired(amount,coins);
+        System.out.println(output);
+    }
+
+    public static int minimumCoinsRequired1(int amount,int[] coins){
+        int max = amount+1;
+        int[] dp = new int[max];
+        Arrays.fill(dp,max);
+
+        dp[0] = 0;
+
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j]<=i){
+                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
+    }
+
+
+    public static int minimumCoinsRequired(int amount,int[] coins){
+        int max = amount+1;
+        int[] dp = new int[max];
+
+        Arrays.fill(dp,max);
+
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j]<=i){
+                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+
+        return dp[amount]>amount?-1:dp[amount];
+    }
+}

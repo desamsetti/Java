fa0a710d7a1c38e58246e77260343ca6968f86b4
Harshith Desamsetti <dharshith1996@gmail.com>
2019-06-18 15:03:08 -0500

    Updated

---
+++ springify/src/main/java/io/ennate/App.java
@@ -0,0 +1,13 @@
+package io.ennate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

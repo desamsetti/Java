346c2afd4c5e544fe5b9e6ea72f4253eb76db97a
Harshith Desamsetti <dharshith1996@gmail.com>
2019-06-25 14:23:19 -0500

    Updated

--- EvenFibanocciNumbers/src/com/company/Main.java
+++ EvenFibanocciNumbers/src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-
+    static HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
     public static void main(String[] args) {
 	// write your code here
         ArrayList<Integer> list1 = new ArrayList<Integer>();
@@ -27,20 +29,44 @@ public class Main {
         System.out.println("The fibanocci value is "+sum);
     }
 
+    public static int fib(int n){
+
+        if(n<=1){
+            return n;
+        }
+        else{
+            if (map.containsKey(n)) {return map.get(n); }
+            else{
+                int nthVal = fib(n-1)+fib(n-2);
+                map.put(n,nthVal);
+                return nthVal;
+                //map.put(n,fib(n-1)+fib(n-2));
+                /*Iterator it = map.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry pairs = (Map.Entry) it.next();
+                    System.out.println(pairs.getKey() + " = " + pairs.getValue());
+                }*/
+                //return fib(n-1)+fib(n-2);
+            }
+        }
+    }
+
 
     public static int fibanocci(int x)
     {
-        if(x==0)
-        {
-            return 0;
+        Map<Integer,Integer> hashMap = new HashMap<Integer,Integer>();
+        if(hashMap.containsKey(x)){
+            return hashMap.get(x);
         }
-        else if(x==1)
-        {
-            return 1;
-        }
-        else
-        {
-            return fibanocci(x-1)+fibanocci(x-2);
+        else {
+            if (x == 0) {
+                return 0;
+            } else if (x == 1) {
+                return 1;
+            } else {
+                hashMap.put(x,fibanocci(x-1)+fibanocci(x-2));
+                return fibanocci(x - 1) + fibanocci(x - 2);
+            }
         }
     }
 

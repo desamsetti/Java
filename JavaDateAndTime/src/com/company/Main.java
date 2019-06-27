2f9511dbeb9c1c2b8e4a607ba0bf9db3f20f1959
Harshith Desamsetti <dharshith1996@gmail.com>
2019-06-27 11:28:28 -0500

    Updated

---
+++ JavaDateAndTime/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int month = 8;
+        int day = 5;
+        int year = 2015;
+        System.out.println(findDay(month,day,year));
+
+    }
+
+    public static String findDay(int month, int day, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek().name();
+    }
+}

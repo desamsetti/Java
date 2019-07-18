fd59039bad00cf5030c30438e332f9cf50c9d537
Harshith Desamsetti <dharshith1996@gmail.com>
2019-07-18 14:21:12 -0500

    Updated

--- ContainsDuplicate/src/com/company/Main.java
+++ ContainsDuplicate/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] inputArray = {1,2,3};
+        int[] inputArray = {1,2,3,1};
         //int[] inputArray = {1,1,1,3,3,4,3,2,4,2};
         boolean output = containsDuplicate1(inputArray);
         System.out.println(output);
@@ -19,6 +19,17 @@ public class Main {
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]){
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicate2(int[] nums){
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
             }
         }
         return false;

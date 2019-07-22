34b64e54a7587d6d5786e4c0c50690c317923ba5
Harshith Desamsetti <dharshith1996@gmail.com>
2019-07-22 10:47:57 -0500

    Added Encapsulation Java Program

---
+++ Udemy/Encapsulation/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*Player player = new Player();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+
+        System.out.println("Remaining health = "+player.healthRemaining());
+
+        damage = 11;
+        player.health = 200;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = "+player.healthRemaining());*/
+
+        EnhancedPlayer player = new EnhancedPlayer("Tim",200,"Sword");
+        System.out.println("Initial health is "+player.getHitPoints());
+    }
+}
---
+++ Udemy/EncapsulationChallenge/src/com/company/Printer.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Printer {
+    private int tonerLevel=100;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean isDuplexPrinter) {
+        if(tonerLevel>-1 && tonerLevel<=100) {
+            this.tonerLevel = tonerLevel;
+        }
+        else{
+            this.tonerLevel = -1;
+        }
+        this.duplex = isDuplexPrinter;
+        this.pagesPrinted = 0;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public boolean isDuplex() {
+        return duplex;
+    }
+
+    public int addToner(int tonerAmount){
+        if(tonerAmount>0 && tonerAmount<=100){
+            if(this.tonerLevel + tonerAmount > 100){
+                return -1;
+            }
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public int printPages(int pages){
+        int pagesToPrint = pages;
+        if(this.duplex){
+            pagesToPrint = (pages/2)+(pages%2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+}

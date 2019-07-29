65613cea314db7cc4347bfa0e9bfbd404f46fcbe
Harshith Desamsetti <dharshith1996@gmail.com>
2019-07-28 19:51:55 -0500

    Updated

--- Udemy/SpeedConverter/src/com/company/Main.java
+++ Udemy/SpeedConverter/src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
         double miles = SpeedConverter.toMilesPerHour(10.5);
+        double miles1 = SpeedConverter.toMilesPerHour(15);
         System.out.println("Miles = "+miles);
+        System.out.println("Miles = "+miles1);
     }
 }
-

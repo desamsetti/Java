5dd5932fe976570e57aa8b354fe9b4f382dbeebc
Harshith Desamsetti <dharshith1996@gmail.com>
2019-07-30 23:52:26 -0500

    Updated

---
+++ Udemy/OOPMasterChallenge/src/com/company/DeluxeBurger.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class DeluxeBurger extends Hamburger{
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips",2.75);
+        super.addHamburgerAddition2("Drink",1.81);
+
+
+    }
+}
--- Udemy/OOPMasterChallenge/src/com/company/Main.java
+++ Udemy/OOPMasterChallenge/src/com/company/Main.java
@@ -10,5 +10,17 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce",0.75);
         hamburger.addHamburgerAddition3("Cheese",1.12);
         System.out.println("Total Burger price is "+hamburger.itemizeHamburger());
+
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon",5.67);
+        healthyBurger.itemizeHamburger();
+        healthyBurger.addHamburgerAddition1("Egg",5.43);
+        healthyBurger.itemizeHamburger();
+        healthyBurger.addHealthAddition1("Lentils",3.41);
+        System.out.println("Total Healthy Burger price is "+healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition1("Should not do this",50.53);
+        db.itemizeHamburger();
     }
 }

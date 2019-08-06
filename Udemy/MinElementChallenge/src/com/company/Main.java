153d70f592ad99aa8fa112647990a9f00c2a8032
Harshith Desamsetti <dharshith1996@gmail.com>
2019-08-06 12:49:07 -0500

    Updated

---
+++ Udemy/MinElementChallenge/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+	// write your code here
+        int[] inputArray = readIntegers(5);
+        System.out.println("The minimum element is :"+findMin(inputArray));
+    }
+
+    public static int[] readIntegers(int count){
+        int inputArray[] = new int[count];
+        for(int i=0;i<count;i++){
+            System.out.print("Enter the element number "+(i+1)+" : ");
+            inputArray[i] = scanner.nextInt();
+            System.out.println();
+        }
+        return inputArray;
+    }
+
+    public static int findMin(int[] inputArray){
+        int minElement = inputArray[0];
+        for(int i=0;i<inputArray.length;i++){
+            if(inputArray[i]<minElement){
+                minElement = inputArray[i];
+            }
+        }
+        return minElement;
+    }
+}

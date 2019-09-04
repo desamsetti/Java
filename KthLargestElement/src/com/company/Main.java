cbb47eed052465484da846005e784846c7e0c3f3
Harshith Desamsetti <dharshith1996@gmail.com>
2019-09-03 21:51:51 -0500

    Updated

--- KthLargestElement/src/com/company/Main.java
+++ KthLargestElement/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        KthLargest kthLargest = new KthLargest(3,new int[]{4,5,8,2});
+        KthLargest kthLargest = new KthLargest(3,new int[]{4,5,8,2,10});
         System.out.println(kthLargest.add(3));
         System.out.println(kthLargest.add(5));
         System.out.println(kthLargest.add(10));
